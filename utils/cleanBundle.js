23b1a2a0b23f379f3cf86ce75b5d49c4340cf143
gabsruff <gabrielromero96@gmail.com>
2025-09-19 18:29:17 -0300

    fix clean bundle

--- utils/cleanBundle.js
+++ utils/cleanBundle.js
@@ -1,12 +1,13 @@
 import { unlink } from "fs/promises";
 import { join } from "path";
+import colors from "yoctocolors";
 
 export async function cleanBundle() {
-  const file = join(process.cwd(), "wp-plugin/index.html");
+  const file = join(process.cwd(), "wp-plugin/assets/index.html");
   try {
     await unlink(file);
     console.log(colors.green("✔ Bundle cleaned."));
   } catch (err) {
-    console.error(`❌ Error cleaning bundle.`);
+    console.error(colors.red(`❌ Error cleaning bundle.`), err);
   }
 }
